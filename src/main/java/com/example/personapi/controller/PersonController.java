0d03f8dca1ab9a7df5a03321e0e101040f03af42
Cleverson Barros <cleversonferreirab@gmail.com>
2020-11-15 09:09:00 -0300

    fixed inconsistence between birthDate and birthDay

--- src/main/java/com/example/personapi/controller/PersonController.java
+++ src/main/java/com/example/personapi/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.example.personapi.dto.request.PersonDTO;
 import com.example.personapi.dto.response.MessageResponseDTO;
 import com.example.personapi.exception.PersonNotFoundException;
 import com.example.personapi.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
