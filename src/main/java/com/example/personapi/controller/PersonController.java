5f77dec1ce194d72c965905289fbbfde7be258cb
Cleverson Barros <cleversonferreirab@gmail.com>
2020-11-14 21:08:02 -0300

    Refactored using person service

--- src/main/java/com/example/personapi/controller/PersonController.java
+++ src/main/java/com/example/personapi/controller/PersonController.java
@@ -1,16 +1,26 @@
 package com.example.personapi.controller;
 
+import com.example.personapi.dto.response.MessageResponseDTO;
 import com.example.personapi.entity.Person;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook() {
-        return "APi test";
+    private PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
     }
 }
